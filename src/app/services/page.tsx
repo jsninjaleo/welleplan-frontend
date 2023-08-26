1dd0f82aa95feeda2789cab9893c8df60cbbe8ae
jsninjaleo <gorayblum123@gmail.com>
2023-08-26 11:30:09 +0200

    add services page and fix font issue

---
+++ src/app/services/page.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import { useRef } from 'react'
+import Input from '@/elements/input'
+import { motion, useInView } from 'framer-motion'
+
+
+export default function ContactUs() {
+  return (
+    <div>
+      <div className="bg-mainBlue grid grid-cols-3 gap-20 pt-96 pb-36 2xl:px-52 px-36 relative">
+        <div className='col-span-3 text-5xl font-bold'>
+            Sevices
+        </div>
+
+        <div>
+            <p className='text-3xl font-bold'>Web development</p>
+        </div>
+        
+        <div>
+            <p className='text-3xl font-bold'>Mobile development</p>
+        </div>
+
+        <div>
+            <p className='text-3xl font-bold'>User interface design</p>
+        </div>
+      </div>
+    </div>
+  )
+}
